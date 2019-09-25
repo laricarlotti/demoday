60a99d6d94d37b003d6cb8729c4eecfa2b57734b
laricarlotti <larissa.carlotti@gmail.com>
2019-09-25 09:08:39 -0300

    Criar primeira versão dos models do banco de dados

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Login(models.Model):
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(default=timezone.now)
+    email = models.CharField(max_length=100, verbose_name='Email')
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    sobrenome = models.CharField(max_length=100, verbose_name='Sobrenome')
+
+    def __str__(self):
+        return self.nome
+
+    
+    
+
