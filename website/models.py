4b11d8e179e9336deb9394de7f4602040ad6a9e4
laricarlotti <larissa.carlotti@gmail.com>
2019-09-25 10:39:30 -0300

    Criar caixas de input no HTML

--- website/models.py
+++ website/models.py
@@ -7,6 +7,7 @@ class Login(models.Model):
     email = models.CharField(max_length=100, verbose_name='Email')
     nome = models.CharField(max_length=100, verbose_name='Nome')
     sobrenome = models.CharField(max_length=100, verbose_name='Sobrenome')
+    usuario = models.CharField(max_length=100, verbose_name='Nome de Usuário')
 
     def __str__(self):
         return self.nome
