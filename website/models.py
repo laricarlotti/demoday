1130231d895bf10b91d6b16124a77c58c7866de7
laricarlotti <larissa.carlotti@gmail.com>
2019-09-27 11:19:28 -0300

    Corrigir unicode no models

--- website/models.py
+++ website/models.py
@@ -2,15 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class User(models.Model):
-    nome = models.CharField(default='',max_length=100, verbose_name='Nome')
+    nome = models.CharField(default='', max_length=100, verbose_name='Nome')
     email = models.CharField(max_length=100, verbose_name='Email', unique=True)
     senha = models.CharField(default= '', max_length=100, verbose_name='Senha')
 
     ativo = models.BooleanField(default=True)
     criado_em = models.DateTimeField(default=timezone.now)
-
-    def __unicode__(self):
-        return self.nome
 
     def __str__(self):
         return self.nome
