c03730e8dc93ed0802527b20d7f6afd405e72213
Ana CLaudia <anapntra@gmail.com>
2019-10-13 22:51:40 -0300

    limpar código

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -5,20 +5,7 @@ let botao = document.querySelector("button");
 let h2 = document.querySelector("h2");
 
 function mostrarMenu(){
-    // nav.style.display = "block";
-    //JEITO 1 
-    // if(nav.classList.contains("visivel")){
-    //     nav.classList.remove("visivel");
-    // }else{
-    //     nav.classList.add("visivel");  
-    // }
-    //JEITO 2
-    // if(!nav.classList.contains("visivel")){
-    //     nav.classList.add("visivel");  
-    // }else{
-    //     nav.classList.remove("visivel");
-    // }
-    //JEITO 3
+  
     main.classList.toggle("visivel");
     menuHamburguer.classList.toggle("ativo");
 }
