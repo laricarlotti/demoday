73aa6ec710e62c802699dd8ebde5779828e27145
Camila Gomes <camila.s.gomes@hotmail.com>
2019-10-21 00:44:30 -0300

    tentativa de fazer JS na pagina de produtos

--- website/static/js/produtos.js
+++ website/static/js/produtos.js
@@ -1,3 +1,51 @@
-// PARA SABER SE JS ESTÁ FUNCIONANDO COLOCAR NO CONSOLE A VARIAVEL E DEVE APARECER O VALOR DELA
 
-// let teste = 'oi'  (teste é a variavel - Oi é o valor)
+// FUNÇÃO FUNCIONANDO PORÉM IMAGENS NÃO APARECEM
+
+// let i = 0;
+// let images = [];
+// let time = 3000;
+
+
+// images[0] = (".batedeira-amarela");
+// images[1] = ('.imagem-principal');
+// images[2] = ('.batedeira-vermelha');
+
+
+// function changeImg(){
+//     document.slide.src = images[i];
+
+
+//     if (i < images.length -1){
+//         i++;
+//     }   else{
+//         i = 0;
+//     }
+//     setTimeout("changeImg()", time);
+// }
+
+// window.onload = changeImg;
+
+
+
+
+
+let botaoMais = document.getElementById('#mais')
+let botaoMenos = document.getElementById('#menos')
+
+// var count = 5;//recebendo o valor 5 que você disse
+// $('#addCount').click(function(){
+//   alert(count);
+//   count++;
+// });
+
+
+let contador = 0;
+
+$('#mais').click(function(){
+    alert(contador);
+    contador++;
+});
+
+function Aumentar(){
+    onclick ()
+}
