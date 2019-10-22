86ffdcc9dbabdfe597d5919f353f3a25826d0e58
Camila Gomes <camila.s.gomes@hotmail.com>
2019-10-22 00:06:14 -0300

    Fazer carrossel produtos funcionar

--- website/static/js/produtos.js
+++ website/static/js/produtos.js
@@ -4,6 +4,7 @@
 let i = 0;
 let images = [];
 let time = 3000;
+// let voltar = document.querySelector('.esquerda')
 
 
 images[0] = document.src= '../static/img/piscina.png';
@@ -11,26 +12,19 @@ images[1] = document.src = '../static/img/batedeira-vermelha.png';
 images[2] = document.src ='../static/img/batedeira-laranja.png';
 
 
-// function TrocarImg(){
-//     document.slide.src = images;
 
-// }
-
-
-// function changeImg(){   FUNCAO OFICIAL 
-//     document.slide.src = images[i];
+function changeImg(){ 
+    document.slide.src = images[i];
 
 
-//     if (i < images.length -1){
-//         i++;
-//     }   else{
-//         i = 0;
-//     }
-// }
+    if (i < images.length -1){
+        i++;
+    }   else{
+        i = 0;
+    }
+}
 
-// window.onload = changeImg;
-
-// onclick = changeImg; --- onclick foi chamado direto na ancora do html
+window = changeImg;
 
 
 
@@ -65,19 +59,19 @@ images[2] = document.src ='../static/img/batedeira-laranja.png';
 // }
 
 
-function aparecerImg(){
-    document.slide.src = '../static/img/piscina.png';
-}
+// function aparecerImg(){
+//     document.slide.src = '../static/img/piscina.png';
+// }
 
-window.onload = aparecerImg;
+// window.onload = aparecerImg;
 
-function voltarImg(){
-    document.slide.scr = '../static/img/batedeira-laranja.png';
-}
+// function voltarImg(){
+//     document.slide.scr = '../static/img/batedeira-laranja.png';
+// }
 
-onclick = voltarImg;
+// onclick = voltarImg;
 
-function passarImg(){
-    document.slide.scr = '../static/img/batedeira-vermelha.png';
-}
+// function passarImg(){
+//     document.slide.scr = '../static/img/batedeira-vermelha.png';
+// }
 
