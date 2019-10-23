1005897296b665d73c872ead60e11b7df57e22d2
Lari Carlotti <larissa.carlotti@gmail.com>
2019-10-23 16:03:08 -0300

    Update wsgi.py

--- projeto_demoday/wsgi.py
+++ projeto_demoday/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'precisei.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_demoday.settings')
 
 application = get_wsgi_application()
