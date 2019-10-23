c28dce5dec7bdf6ab250f5056b2ae23b8aea6346
LariCarlotti <larissa.carlotti@gmail.com>
2019-10-23 10:41:49 -0300

    Adicionar project_root e static_root no settings.py

--- projeto_demoday/settings.py
+++ projeto_demoday/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 django_heroku.settings(locals())
