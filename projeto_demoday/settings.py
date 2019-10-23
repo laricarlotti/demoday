7da3e28154d9e122494a103e426fabdf0ff8afc5
LariCarlotti <larissa.carlotti@gmail.com>
2019-10-23 10:25:32 -0300

    Change staticfiles in settings.py

--- projeto_demoday/settings.py
+++ projeto_demoday/settings.py
@@ -120,5 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings(locals())
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+django-heroku.settings(locals())
