dd0386a13e60c459b40170daace6f599991be945
LariCarlotti <larissa.carlotti@gmail.com>
2019-10-23 10:30:14 -0300

    Lugar extra para o collectstatic encontrar os staticfiles

--- projeto_demoday/settings.py
+++ projeto_demoday/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'
 
-heroku config:set DISABLE_COLLECTSTATIC=1
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django-heroku.settings(locals())
